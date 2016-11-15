54c3ffab2b02c830ca2419359760546f108a3bca
Minh Thành Nguyễn <Minh Thnh Nguyn>
2016-11-15 15:46:56 +0700

    First push

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -1,6 +1,6 @@
 import { Component, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Service } from '../share/variables';
 /*
   Generated class for the LoginPage page.
 
@@ -10,31 +10,30 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-login',
   templateUrl: 'login-page.html',
- 
+
   animations: [
- 
+
     //For the logo
     trigger('flyInBottomSlow', [
       state('in', style({
         transform: 'translate3d(0,0,0)'
       })),
       transition('void => *', [
-        style({transform: 'translate3d(0,2000px,0'}),
+        style({ transform: 'translate3d(0,2000px,0' }),
         animate('2000ms ease-in-out')
       ])
     ]),
- 
+
     //For the background detail
     trigger('flyInBottomFast', [
       state('in', style({
         transform: 'translate3d(0,0,0)'
       })),
       transition('void => *', [
-        style({transform: 'translate3d(0,2000px,0)'}),
+        style({ transform: 'translate3d(0,2000px,0)' }),
         animate('1000ms ease-in-out')
       ])
     ]),
- 
     //For the login form
     trigger('bounceInBottom', [
       state('in', style({
@@ -42,34 +41,46 @@ import { NavController } from 'ionic-angular';
       })),
       transition('void => *', [
         animate('2000ms 200ms ease-in', keyframes([
-          style({transform: 'translate3d(0,2000px,0)', offset: 0}),
-          style({transform: 'translate3d(0,-20px,0)', offset: 0.9}),
-          style({transform: 'translate3d(0,0,0)', offset: 1}) 
+          style({ transform: 'translate3d(0,2000px,0)', offset: 0 }),
+          style({ transform: 'translate3d(0,-20px,0)', offset: 0.9 }),
+          style({ transform: 'translate3d(0,0,0)', offset: 1 })
         ]))
       ])
     ]),
- 
+
     //For login button
     trigger('fadeIn', [
       state('in', style({
         opacity: 1
       })),
       transition('void => *', [
-        style({opacity: 0}),
+        style({ opacity: 0 }),
         animate('1000ms 2000ms ease-in')
       ])
     ])
   ]
 })
 export class LoginPage {
- 
+
   logoState: any = "in";
   cloudState: any = "in";
   loginState: any = "in";
   formState: any = "in";
- 
-  constructor(public navCtrl: NavController) {
- 
+  // username + password
+  username: string;
+  password: string;
+  constructor(public navCtrl: NavController, private service: Service) {
   }
- 
+
+  Login() {
+    this.service.LoginToSever(this.username, this.password, `/Static/POST_Login`)
+      .then(data => {
+        if (data == "true") {
+          this.service.ShowToastOK("Đăng nhập thành công")
+          return this.navCtrl.push(LoginPage)
+        }else{
+          return 
+        }
+      })
+  }
 }
---
+++ src/pages/share/variables.ts
@@ -0,0 +1,49 @@
+import { Http, URLSearchParams } from '@angular/http';
+
+import { Injectable } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+import { } from 'rxjs'
+@Injectable()
+export class Service {
+    private url = `http://test3.hutech.edu.vn/quantrac/api/Static/`
+    constructor(private _http: Http, private _toast: ToastController) {
+        this._http = _http
+    }
+
+    public LoginToSever(user: any, pass: any, url: string): Promise<any> {
+        let body = new URLSearchParams()
+        body.set("Username", user)
+        body.set("pass", pass)
+        return this._http.post(`${this.url}url`, body)
+            .toPromise()
+            .then(respone => respone['_body'])
+            .catch(this.handleError)
+    }
+
+    /**
+     * Get_DataInfo_Service
+     */
+    public GetData(url: string) {
+        return this._http.get(`${this.url}Get_InfoLogin?username=system`)
+            .toPromise()
+            .then(respone => { respone['_body'] })
+            .catch(this.handleError)
+    }
+
+    private handleError(error: Error): Promise<any> {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        console.error(error);
+        return Promise.reject(error.message || error);
+        // return Observable.throw(error.json().error || 'Server error');
+
+    }
+    public ShowToastOK = (mess: string) => {
+        let toast = this._toast.create({
+            position: `middle`,
+            duration: 2000,
+            message: mess
+        })
+        toast.present()
+    }
+}

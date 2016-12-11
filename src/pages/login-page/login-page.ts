d8ece094f90c38fd92f1c31747d50b5f08f02ec4
tunglunnho <bebinh.hutech.95@gmail.com>
2016-12-11 09:41:28 +0700

    merge v2

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -3,7 +3,8 @@ import { NavController } from 'ionic-angular';
 import { LoginService } from '../shared/services/login-page.service';
 import { Storage } from '@ionic/storage';
 import { HomePage } from '../homepage/homepage';
-
+import {TinTucPage} from '../tintuc/tintuc';
+import {Facebook} from 'ionic-native';
 /*
   Generated class for the LoginPage page.
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
@@ -86,28 +87,44 @@ export class LoginPage {
                 default:
             }
             console.log(`${this.username}${this.password}`)
-
-            if (this.username && this.password)
-                this.service.LoginToSever(this.username, this.password)
-                    .then(result => {
-                        if (result !== 'Khong Co') {
-                            this.service.ShowToastOK("Dang Nhap Thanh Cong", { position: 'top' })
-                            this.IDuser = result._body;
-                            console.log("id user:" + this.IDuser);
-                            this.navCtrl.push(HomePage, { id: this.IDuser });
-                        }
-                        else {
-                            console.log(result)
-                            this.service.ShowToastOK("Dang nhap ko thanh cong", { position: 'top', duration: 3000 })
-                            return
-                        }
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
+             //login bebinh
+             this.LoginRouterPage();
+            // if (this.username && this.password)
+            //     this.service.LoginToSever(this.username, this.password)
+            //         .then(result => {
+            //             if (result !== 'Khong Co') {
+            //                 this.service.ShowToastOK("Dang Nhap Thanh Cong", { position: 'top' })
+            //                 this.IDuser = result._body;
+            //                 console.log("id user:" + this.IDuser);
+            //                 this.navCtrl.push(HomePage, { id: this.IDuser });
+            //             }
+            //             else {
+            //                 console.log(result)
+            //                 this.service.ShowToastOK("Dang nhap ko thanh cong", { position: 'top', duration: 3000 })
+            //                 return
+            //             }
+            //         })
+            //         .catch(err => {
+            //             console.log(err)
+            //         })
         })
     }
 
+     //Login dem count>0 vo thang tin tuc, count==0 vo homepage
+  LoginRouterPage = () => {
+    this.service.GetLogin(this.username, this.password).then(res => {
+      this.IDuser = res;
+      this.service.GetCount(res).then(data => {
+        if (data == 0) {
+          this.navCtrl.setRoot(HomePage, this.IDuser);
+        }
+        else {
+          this.navCtrl.setRoot(TinTucPage, this.IDuser);
+        }
+
+      })
+    });
+  }
 
     Login = () => {
         this.service.LoginToSever(this.username, this.password)
@@ -122,8 +139,12 @@ export class LoginPage {
                         this.storage.set("Password", this.password);
                         this.storage.set("Checkbox", this.save);
                         this.navCtrl.push(HomePage, { id: this.IDuser });
+                          //Login Bebinh 
+                         this.LoginRouterPage();
+
                     } else {
-                        this.navCtrl.push(HomePage, { id: this.IDuser });
+                        //Login Bebinh
+                          this.LoginRouterPage();
                     }
                 }
 
@@ -138,4 +159,49 @@ export class LoginPage {
     Change = () => {
         console.log(`save: ${this.save}`)
     }
+     LoginFacebook() {
+    //test
+    Facebook.login(['email']).then((response) => {
+      let token = response.authResponse.accessToken;
+      // alert(JSON.stringify(response));
+      Facebook.api('/' + response.authResponse.userID + '?fields=id,name,email', []).then((result) => {
+        let name = result.name;
+        let email = result.email;
+        let Facebook = response.authResponse.userID;
+        this.service.GetCountFacebook(Facebook).then(resGet => {
+          console.log("resGet:" + resGet);
+          if (resGet > 0) {
+            this.service.ShowToastOK("Đăng nhập thành công", { position: 'top', duration: 3000 })
+            this.navCtrl.setRoot(HomePage);
+          }
+          else {
+            this.service.InserUserFacebook(Facebook, name, email, token).then(resInsert => {
+              this.service.ShowToastOK("Đăng nhập thành công", { position: 'top', duration: 3000 })
+              this.service.GetCountFacebook(Facebook).then(resGetID => {
+                this.IDuser = resGetID;
+                console.log("id moi=" + this.IDuser);
+                this.navCtrl.setRoot(HomePage, this.IDuser);
+              });
+            })
+          }
+        }).catch(err => {
+          this.service.InserUserFacebook(Facebook, name, email, token).then(resInsert => {
+            this.service.ShowToastOK("Đăng nhập thành công", { position: 'top', duration: 3000 })
+            console.log("id moi:" + this.IDuser);
+            this.service.GetCountFacebook(Facebook).then(resGetID => {
+              this.IDuser = resGetID;
+              console.log("id moi=" + this.IDuser);
+              this.navCtrl.setRoot(HomePage, this.IDuser);
+            });
+          });
+        });
+        // coi thử xem có nên làm ghi nhớ hem
+        this.username=Facebook;
+        this.password=Facebook;
+        this.storage.set("TaiKhoan",this.username);
+        this.storage.set("Password",this.password);
+        // coi thử xem có nên làm ghi nhớ hem
+      }).catch(err => { this.service.ShowToastOK("Đăng nhập thất bại xin bạn thử lại", { position: 'top', duration: 3000 }) });
+    }).catch(err => { this.service.ShowToastOK("Đăng nhập thất bại xin bạn thử lại", { position: 'top', duration: 3000 }) });
+  }
 }
